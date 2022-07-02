0cde7e84a7cc56496440350081f8a6f46536934f
Hyan Lee <hyan23lee@hotmail.com>
2022-07-02 18:05:18 +0800

    create file view and return as slice

--- backend/storage/mmap_test.go
+++ backend/storage/mmap_test.go
@@ -1,18 +1,26 @@
 package storage
 
 import (
-	"path"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestMmap(t *testing.T) {
-	dir := t.TempDir()
-	filename := path.Join(dir, "test")
+	// dir := t.TempDir()
+	// filename := path.Join(dir, "test")
+	filename := "test.txt"
 
 	f := MmapFile{}
 	err := f.Init(filename)
 	assert.Nil(t, err)
 	defer f.Close()
+
+	s := f.AsSlice()
+	fmt.Println(string(s))
+	for i := range s {
+		s[i] = 't'
+	}
+	f.Flush()
 }
--- backend/storage/mmap_win.go
+++ backend/storage/mmap_win.go
@@ -4,17 +4,23 @@ package storage
 
 import (
 	"fmt"
+	"reflect"
 	"syscall"
+	"unsafe"
 )
 
 // MmapFile ...
 type MmapFile struct {
-	h syscall.Handle
+	h    syscall.Handle
+	obj  syscall.Handle
+	addr uintptr
 }
 
 // Init ...
-func (f *MmapFile) Init(filename string) error {
+func (f *MmapFile) Init(filename string) (err error) {
 	f.h = syscall.InvalidHandle
+	f.obj = syscall.InvalidHandle
+	f.addr = 0
 	name, err := syscall.UTF16PtrFromString(filename)
 	if err != nil {
 		return err
@@ -24,25 +30,85 @@ func (f *MmapFile) Init(filename string) error {
 	// FILE_FLAG_SEQUENTIAL_SCAN
 	// FILE_FLAG_WRITE_THROUGH
 	h, err := syscall.CreateFile(name, syscall.GENERIC_READ|syscall.GENERIC_WRITE,
-		syscall.FILE_SHARE_READ, nil, syscall.CREATE_ALWAYS, syscall.FILE_ATTRIBUTE_NORMAL, 0)
+		0, nil, syscall.OPEN_ALWAYS, syscall.FILE_ATTRIBUTE_NORMAL, 0)
 	if err != nil {
 		return err
 	}
+	defer func() {
+		if err != nil {
+			syscall.CloseHandle(h)
+		}
+	}()
 
+	obj, err := syscall.CreateFileMapping(h, nil, syscall.PAGE_READWRITE, 0, 1024, nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			syscall.CloseHandle(obj)
+		}
+	}()
+
+	addr, err := syscall.MapViewOfFile(obj, syscall.FILE_MAP_WRITE, 0, 0, 0)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			syscall.UnmapViewOfFile(addr)
+		}
+	}()
+
+	f.obj = obj
 	f.h = h
+	f.addr = addr
 	// syscall.CreateFileMapping()
 	// syscall.MapViewOfFile()
+
+	// syscall.UnmapViewOfFile()
+	// syscall.SetFileTime()
 	return nil
 }
 
 // Close ...
 func (f *MmapFile) Close() {
+	if f.addr != 0 {
+		err := syscall.UnmapViewOfFile(f.addr)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			f.addr = 0
+		}
+	}
+	if f.obj != syscall.InvalidHandle {
+		err := syscall.CloseHandle(f.obj)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			f.obj = syscall.InvalidHandle
+		}
+	}
 	if f.h != syscall.InvalidHandle {
 		err := syscall.CloseHandle(f.h)
 		if err != nil {
 			fmt.Println(err)
-			return
+		} else {
+			f.h = syscall.InvalidHandle
 		}
-		f.h = syscall.InvalidHandle
 	}
+}
+
+// AsSlice ...
+func (f *MmapFile) AsSlice() []byte {
+	sh := reflect.SliceHeader{}
+	sh.Data = f.addr
+	sh.Cap = 1024
+	sh.Len = 1024
+	return *((*[]byte)(unsafe.Pointer(&sh)))
+}
+
+// Flush ...
+func (f *MmapFile) Flush() error {
+	return syscall.FlushViewOfFile(f.addr, 1024)
 }
