a92054e012e037da665a76788dc356bb3089ab6c
Hyan Lee <hyan23lee@hotmail.com>
2022-07-06 21:36:22 +0800

    implement MapWithWal

--- backend/storage/map_with_wal.go
+++ backend/storage/map_with_wal.go
@@ -1,29 +1,58 @@
 package storage
 
-import "fmt"
+import (
+	"fmt"
+)
 
 // MapWithWal ...
 type MapWithWal struct {
+	log *LogFile
+	m   map[string]string
+}
+
+func (s *MapWithWal) Init(log *LogFile) {
+	s.log = log
+	s.m = map[string]string{}
 }
 
 func (s *MapWithWal) Save(key string, value string) error {
+	err := s.log.Append(&LogEntry{Op: int32(Op_Modify), Key: key, Value: value})
+	if err != nil {
+		return err
+	}
+	s.m[key] = value
 	return nil
 }
 
 func (s *MapWithWal) Del(key string) error {
+	err := s.log.Append(&LogEntry{Op: int32(Op_Del), Key: key})
+	if err != nil {
+		return err
+	}
+	delete(s.m, key)
 	return nil
 }
 
 func (s *MapWithWal) Has(key string) (bool, error) {
+	if _, ok := s.m[key]; ok {
+		return true, nil
+	}
 	return false, nil
 }
 
 func (s *MapWithWal) Load(key string) (string, error) {
+	if val, ok := s.m[key]; ok {
+		return val, nil
+	}
 	return "", fmt.Errorf("not exist")
 }
 
 func (s *MapWithWal) All() ([][2]string, error) {
-	return nil, nil
+	records := [][2]string{}
+	for k, v := range s.m {
+		records = append(records, [2]string{k, v})
+	}
+	return records, nil
 }
 
 func _() {
