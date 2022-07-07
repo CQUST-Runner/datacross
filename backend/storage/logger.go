29a5c664e1c1ae3af61603f834fb7e959448eb62
Hyan Lee <hyan23lee@hotmail.com>
2022-07-07 18:02:33 +0800

    added Logger

---
+++ backend/storage/logger.go
@@ -0,0 +1,51 @@
+package storage
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type Logger interface {
+	Category(catetory string) Logger
+	Warn(format string, args ...interface{})
+	Error(format string, args ...interface{})
+	Info(format string, args ...interface{})
+}
+
+type builtinLoggerAdapter struct {
+	l   *log.Logger
+	cat string
+	out io.Writer
+}
+
+func newLogger(out io.Writer, cat string) *builtinLoggerAdapter {
+	return &builtinLoggerAdapter{
+		l:   log.New(out, cat, log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile|log.Lmsgprefix),
+		cat: cat,
+		out: out,
+	}
+}
+
+func (a *builtinLoggerAdapter) Category(catetory string) Logger {
+	// TODO: make sure a.out thread-safe
+	return newLogger(a.out, catetory)
+}
+
+func (a *builtinLoggerAdapter) Warn(format string, args ...interface{}) {
+	a.l.Printf("WARN "+format, args...)
+}
+
+func (a *builtinLoggerAdapter) Error(format string, args ...interface{}) {
+	a.l.Printf("ERROR "+format, args...)
+}
+
+func (a *builtinLoggerAdapter) Info(format string, args ...interface{}) {
+	a.l.Printf("INFO "+format, args...)
+}
+
+var logger Logger = newLogger(os.Stdout, "Core")
+
+func SetLogger(l Logger) {
+	logger = l
+}
