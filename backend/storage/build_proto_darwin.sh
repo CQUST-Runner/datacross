92ba322f7503316a2ab35e0f94e3988fb8f81b20
Hyan Lee <hyan23lee@hotmail.com>
2022-07-13 20:53:22 +0800

    build proto for mac

---
+++ backend/storage/build_proto_darwin.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gopath=`go env | grep GOPATH | cut -d'"' -f 2`"/bin"
+export PATH=$gopath:$PATH
+../../third-party/protoc-21.2-osx-universal_binary/bin/protoc --gofast_out=. proto.proto
